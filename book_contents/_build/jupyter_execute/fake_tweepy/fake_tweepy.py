4986e73c9ca27f065b52d406fed4951962cc77b0
Kyle <kmthayer@uw.edu>
2022-09-14 14:49:00 -0700

    update fake tweepy

--- book_contents/_build/jupyter_execute/fake_tweepy/fake_tweepy.py
+++ book_contents/_build/jupyter_execute/fake_tweepy/fake_tweepy.py
@@ -9,14 +9,42 @@
 
 from types import SimpleNamespace
 
+from IPython.display import HTML, Image, display
+import html
+
+
 
 # In[2]:
 
 
-tweepy = SimpleNamespace(
-    Client = lambda bearer_token="", consumer_key="", consumer_secret="", access_token="", access_token_secret="" : SimpleNamespace(
-      create_tweet = lambda text="" : print(text)
+def print_info(text):
+    display(
+        HTML(
+            "<strong style='color:darkred'>" + 
+            html.escape(text) + 
+            "</strong>"
+        )
     )
+
+
+# In[3]:
+
+
+print_info("Fake tweepy is replacing the tweepy library. Fake Tweepy doesn't need real passwords, and prevents you from accessing real twitter")
+
+def print_tweet(text=""):
+    print_info("Fake Tweepy is pretending to post this tweet (note: real tweepy shows no output here when a tweet is posted): ")
+    print(text)
+
+def client_creator(bearer_token="", consumer_key="", consumer_secret="", access_token="", access_token_secret="" ):
+    print_info("Fake Tweepy is pretending to log in to twitter")
+    return SimpleNamespace(
+      create_tweet = print_tweet
+    )
+
+
+tweepy = SimpleNamespace(
+    Client = client_creator
 )
 
 
