9f91613a24f9d80b96488839455cf733bc01b723
Kyle <kmthayer@uw.edu>
2022-10-16 23:15:24 -0700

    Finish most non-programming parts of data mining chapter.

---
+++ book_contents/_build/jupyter_execute/ch08_data_mining/06_sentiment_analysis/01_demo_sentiment_and_loop_counts.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Demo: Sentiment Analysis and Loop Counts
---
+++ book_contents/_build/jupyter_execute/ch08_data_mining/06_sentiment_analysis/02_practice_sentiment_and_loop_counts.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Practice: Sentiment Analysis and Loop Counts
