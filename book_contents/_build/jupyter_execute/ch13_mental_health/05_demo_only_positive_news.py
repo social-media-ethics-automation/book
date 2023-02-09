7bfbc6d4e047a65e9a8daf4717de8e4a133822f5
Kyle <kmthayer@uw.edu>
2023-02-09 10:07:51 -0800

    update mental health code demo to be reddit specific

--- book_contents/_build/jupyter_execute/ch13_mental_health/05_demo_only_positive_news.py
+++ book_contents/_build/jupyter_execute/ch13_mental_health/05_demo_only_positive_news.py
@@ -5,48 +5,48 @@
 # 
 # Let's look at something we could try to do to improve the mental health for our users: Only show positive news!
 # 
-# We'll use sentiment analysis again, but this time we'll do a search for news from a news account, but only display the tweets with a positive sentiment.
+# We'll use sentiment analysis again, but this time we'll get posts from the news subreddit, but only display the tweets with a positive sentiment.
 # 
 # Would this actually improve someone's mental health? It's hard to say! But we can see something that we might try out if we wanted to improve mental health of our users.
 
-# ## Normal Tweepy Set-Up
+# ## Normal Reddit PRAW Setup
 
 # In[1]:
 
 
-# make sure tweepy library is installed
-import tweepy
+import praw
 
 
-# (optional) use the fake version of tweepy, so you don’t have to use real twitter developer access passwords
+# (optional) make a fake praw connection with the fake_praw library
+# 
+# For testing purposes, we've added this line of code, which loads a fake version of praw, so it wont actually connect to reddit. __If you want to try to actually connect to reddit, don't run this line of code.__
 
 # In[2]:
 
 
-get_ipython().run_line_magic('run', '../fake_tweepy/fake_tweepy.ipynb')
+get_ipython().run_line_magic('run', '../fake_apis/fake_praw.ipynb')
 
 
 # In[3]:
 
 
 # Load all your developer access passwords into Python
-# TODO: Put your twitter account's special developer access passwords below:
-bearer_token = "n4tossfgsafs_fake_bearer_token_isa53#$%$"
-consumer_key = "sa@#4@fdfdsa_fake_consumer_key_$%DSG#%DG"
-consumer_secret = "45adf$T$A_fake_consumer_secret_JESdsg"
-access_token = "56sd5Ss4tsea_fake_access_token_%YE%hDsdr"
-access_token_secret = "j^$dr_fake_consumer_key_^A5s#DR5s"
+# TODO: Put your reddit username, password, and special developer access passwords below:
+username="fake_reddit_username"
+password="sa@#4*fdf_fake_password_$%DSG#%DG"
+client_id="45adf$TW_fake_client_id_JESdsg1O"
+client_secret="56sd_fake_client_secret_%Yh%"
 
 
 # In[4]:
 
 
-# Give the tweepy code your developer access passwords so
-# it can perform twitter actions
-client = tweepy.Client(
-   bearer_token=bearer_token,
-   consumer_key=consumer_key, consumer_secret=consumer_secret,
-   access_token=access_token, access_token_secret=access_token_secret
+# Give the praw code your reddit account info so
+# it can perform reddit actions
+reddit = praw.Reddit(
+    username=username, password=password,
+    client_id=client_id, client_secret=client_secret,
+    user_agent="a custom python script"
 )
 
 
@@ -61,44 +61,50 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
 
 
-# ## Code to search and display news tweets
-# Now let's make code that will do a search for news tweets (tweets from a fake_news_site), and display all of them. We will then make a modified version below to compare the results.
+# ## Code to search and display news
+# Now let's make code that will get submissions from the news subreddit (or from a fake_news_site), and display all of them. We will then make a modified version below to compare the results.
 
 # In[6]:
 
 
-query = 'from:fake_news_site'
-tweets = client.search_recent_tweets(query=query, max_results=10)
+# Look up the subreddit "news", then find the "hot" list, getting up to 10 submission
+submissions = reddit.subreddit("news").hot(limit=10)
 
-# go through each tweet
-for tweet in tweets.data:
-    print(tweet.text)
+# Turn the submission results into a Python List
+submissions_list = list(submissions)
+
+# go through each reddit submission
+for submission in submissions_list:
+    print(submission.title)
     print()
 
 
-# ## Search through tweets and only display good news
-# Now we will make a different version of the code that computes the sentiment of each tweet and only displays the ones with positive sentiment.
+# ## Search through news submissions and only display good news
+# Now we will make a different version of the code that computes the sentiment of each submission title and only displays the ones with positive sentiment.
 
 # In[7]:
 
 
-query = 'from:fake_news_site'
-tweets = client.search_recent_tweets(query=query, max_results=20)
+# Look up the subreddit "news", then find the "hot" list, getting up to 10 submission
+submissions = reddit.subreddit("news").hot(limit=10)
 
-# go through each tweet
-for tweet in tweets.data:
+# Turn the submission results into a Python List
+submissions_list = list(submissions)
+
+# go through each reddit submission
+for submission in submissions_list:
     
     #calculate sentiment
-    tweet_sentiment = sia.polarity_scores(tweet.text)["compound"]
-
-    if(tweet_sentiment > 0):
-        print(tweet.text)
+    title_sentiment = sia.polarity_scores(submission.title)["compound"]
+    
+    if(title_sentiment > 0):
+        print(submission.title)
         print()
 
 
-# ## Try it out on real twitter
-# If you want, you can skip the fake_tweepy step and try it out on real twitter with a query like "from:npr", "from:msnbc", "from:cnn", etc.
+# ## Try it out on real Reddit
+# If you want, you can skip the fake_praw step and try it out on real Reddit, from whatever subreddit you want
 # 
 # Did it work like you expected?
 # 
-# You can also only show negative sentiment tweets (sentiment < 0) if you want to see only bad news.
+# You can also only show negative sentiment submissions (sentiment < 0) if you want to see only bad news.
--- book_contents/_build/jupyter_execute/fake_apis/fake_praw.py
+++ book_contents/_build/jupyter_execute/fake_apis/fake_praw.py
@@ -759,6 +759,88 @@ def science_hot(limit=10):
     return iter(submission_list)
 
 
+def news_hot(limit=10):
+    submission_list = [
+        SimpleNamespace(
+            id = "54oi6refs",
+            title = "Breaking news: A lovely cat took a nice long nap today!", 
+            author = AuthorSimplishNamespace(
+                name="fake_user",
+                link_karma= 3,
+                comment_karma= 4,
+                has_verified_email = False,
+                is_mod = False,
+                is_gold = False                
+            ),
+            edited = False,
+            created_utc = 1673327425,
+            score = 10,
+            upvote_ratio = .5,
+            num_comments = 9,
+            selftext = "",
+            url = "example.com/fake_news_story"
+        ),
+        SimpleNamespace(
+            id = "56etffew3s",
+            title = "Breaking news: Someone said a really mean thing on the internet today!", 
+            author = AuthorSimplishNamespace(
+                name="pretend_user",
+                link_karma= 13,
+                comment_karma= 45,
+                has_verified_email = False,
+                is_mod = True,
+                is_gold = False    
+            ),
+            edited = True,
+            created_utc = 1673327625,
+            score = 10,
+            upvote_ratio = .9,
+            num_comments = 1,
+            selftext = "",
+            url = "example.com/pretend_story.html"
+        ),
+        SimpleNamespace(
+            id = "a443t43",
+            title = "Breaking news: Some grandparents made some yummy cookies for all the kids to share!", 
+            author = AuthorSimplishNamespace(
+                name="imaginary_user",
+                link_karma= 4,
+                comment_karma= 10,
+                has_verified_email = True,
+                is_mod = True,
+                is_gold = True    
+            ),
+            edited = False,
+            created_utc = 1673367625,
+            score = 50,
+            upvote_ratio = .7,
+            num_comments = 5,
+            selftext = "",
+            url = "example.com/imaginary_story.html"
+        ),
+        SimpleNamespace(
+            id = "324lkji34oi324",
+            title = "Breaking news: All the horrors of the universe revealed at last!", 
+            author = AuthorSimplishNamespace(
+                name="imaginary_user",
+                link_karma= 4,
+                comment_karma= 10,
+                has_verified_email = True,
+                is_mod = True,
+                is_gold = True    
+            ),
+            edited = False,
+            created_utc = 1673367635,
+            score = 50,
+            upvote_ratio = .7,
+            num_comments = 5,
+            selftext = "",
+            url = "example.com/imaginary_story.html"
+        )
+    ]
+    return iter(submission_list)
+
+
 # In[12]:
 
 
@@ -780,6 +862,11 @@ def subreddit(subreddit_name):
         return SimpleNamespace(
           submit = submit,
           hot = science_hot
+        )
+    elif subreddit_name == "news":
+        return SimpleNamespace(
+          submit = submit,
+          hot = news_hot
         )
 
 
